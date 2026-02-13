4c3d92340a59aa3c3f6fa217b34e304212cd1f25
Vasanth <vasanthagowda91@gmail.com>
2026-02-13 13:49:43 +0530

    redirection changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,12 @@ import { supabase } from "@/lib/supabaseClient"
 export default function Home() {
 
   const login = async () => {
+    alert(window.location.origin);
+    
   await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/dashboard`
+      redirectTo: `${window.location.origin}`
     },
   })
 }
